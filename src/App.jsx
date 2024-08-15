e59b058913714d1f53dc383569aaa319a92011d9
SagarRathore1 <sagarrathore.fea@gmail.com>
2024-08-15 22:32:00 +0530

    white  background

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-blue-300  selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      {/* <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div> */}
+      </div>
       </div>
       
       
